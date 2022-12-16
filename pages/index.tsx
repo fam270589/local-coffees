29c7f9722485a78c3a49272799b0624c21942088
fam270589 <fam270589@gmail.com>
2022-12-16 17:00:03 +0700

    getStaticProps intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,34 @@ import Image from "next/image";
 
 import styles from "../styles/Home.module.css";
 import Banner from "../components/Banner";
+import Card from "../components/Card";
+import coffeeStoresData from "../data/coffee-stores.json";
 
-export default function Home() {
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+
+type Data = {
+	id: number;
+	name: string;
+	imgUrl: string;
+};
+
+export const getStaticProps: GetStaticProps<{ coffeeStores: Data[] }> = async (
+	context
+) => {
+	// const res = await fetch('https://.../posts')
+	// const posts: Post[] = await res.json()
+	const coffeeStores: Data[] = coffeeStoresData;
+
+	return {
+		props: {
+			coffeeStores,
+		},
+	};
+};
+
+export default function Home(
+	props: InferGetStaticPropsType<typeof getStaticProps>
+) {
 	const handleBannerOnClick = () => {};
 
 	return (
@@ -15,17 +41,37 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<Banner buttonText="View stores nearby" onClick={handleBannerOnClick} />
-				<div className={styles.heroImage}>
-					<Image
-						priority
-						className={styles.image}
-						src={"/static/hero.png"}
-						alt="coffee"
-						width={500}
-						height={500}
+				<div className={styles.top}>
+					<Banner
+						buttonText="View stores nearby"
+						onClick={handleBannerOnClick}
 					/>
+					<div className={styles.heroImage}>
+						<Image
+							priority
+							className={styles.image}
+							src={"/static/hero.png"}
+							alt="coffee"
+							width={500}
+							height={500}
+						/>
+					</div>
 				</div>
+				{props.coffeeStores.length > 0 && (
+					<>
+						<h2 className={styles.heading2}>Toronto stores</h2>
+						<div className={styles.cardLayout}>
+							{props.coffeeStores.map((store) => (
+								<Card
+									key={store.id}
+									name={store.name}
+									image={store.imgUrl}
+									href={`/coffee-store/${store.id}`}
+								/>
+							))}
+						</div>
+					</>
+				)}
 			</main>
 		</div>
 	);
